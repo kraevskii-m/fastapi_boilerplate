1b6c702ebf1c2d1e53fb4a50d5a49149dafeaf9e
Mikhail Kraevskii <kramihm@gmail.com>
2023-07-08 23:36:24 +0700

    refactoring

--- core/api/profile/__init__.py
+++ core/api/profile/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from core.api.profile.id import router as id_router
+from core.api.profile.curremt import router as id_router
 
 router = APIRouter(prefix="/profile")
 router.include_router(id_router)
